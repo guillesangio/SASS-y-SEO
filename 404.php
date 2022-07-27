5efde5df02876e088a0943f864d314bd42d98665
guillesangio <guillesangio@gmail.com>
2022-07-27 20:40:54 -0300

    Proyecto final 1

---
+++ 404.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Error 404</title>
+</head>
+<body>
+	<div class="container-404">
+		<h2>Error 404</h2>
+		<h3>Esta pagina no se encuentra disponible</h3>
+		<h3>Tuvimos dificultades con la pagina no con la cocina</h3>
+		<img src="../img/error404.png" alt="letrero de pizza con tubos led">
+		<a href="index.html"	class="boton_index">VOLVER AL INICIO</a>
+	</div>
+</body>
+</html>
