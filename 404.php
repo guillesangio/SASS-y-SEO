a4080e5a72609786d07a5852a4264ff07e1c29ed
guillesangio <guillesangio@gmail.com>
2022-07-28 17:09:47 -0300

    404

--- 404.php
+++ 404.php
@@ -4,14 +4,111 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Error 404</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	<!--Fin Bootstrap CSS-->
+	<link rel="shortcut icon" href="img/favicon2.ico">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<div class="container-404">
+<!--inicia nav-->
+	<nav class="navbar navbar-expand-lg bg-black sticky-top">
+	  	<div class="container-fluid">
+	    	<a class="navbar-brand" href="index.html">
+	    	<img src="img/logo.svg"  alt="logo de Papin Pizzas - Comer como en casa" id="img_logo">
+	    	</a>
+	    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    	<span class="navbar-toggler-icon"></span>
+	    	</button>
+	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    	<ul class="navbar-nav ms-auto ms-2 mb-lg-0">
+	        	<li class="nav-item">
+	        		<a class="nav-link" href="views/nosotros.html">NOSOTROS</a>
+	        	</li>
+	        	<li class="nav-item">
+	          		<a class="nav-link" href="views/menu.html">MENU</a>
+	        	</li>
+	        	<li class="nav-item">
+	          		<a class="nav-link" href="views/galeria.html">GALERIA</a>
+	        	</li>
+	        	<li class="nav-item">
+	          		<a class="nav-link" href="views/contacto.html">CONTACTO</a>
+	        	</li>        
+	    </div>
+	  	</div>
+	</nav>
+<main>
+	<div class="container_404">
 		<h2>Error 404</h2>
-		<h3>Esta pagina no se encuentra disponible</h3>
-		<h3>Tuvimos dificultades con la pagina no con la cocina</h3>
-		<img src="../img/error404.png" alt="letrero de pizza con tubos led">
-		<a href="index.html"	class="boton_index">VOLVER AL INICIO</a>
+		<h3>Esta pagina no se encuentra disponible en estos momentos</h3> 
+		<h3>la cocina si   !</h3>
+		<h3>Te esperamos en el inicio nuevamente</h3>
 	</div>
+		<a href="index.html"	class="boton_index">VOLVE AL INICIO</a>
+</main>
+	<footer class="pie-pagina" id="pie">
+		<div class="logo_footer">
+			<div class="box1">
+				<a href="index.html">
+					<img src="img/logo.png" alt="logo de Papin Pizzas - Comer como en casa" class="logo_index" id="logo">
+				</a>
+			</div>
+			<div class="box" class="contacto-papin">
+				<h2 class="text-center">CONTACTO</h2>
+				<ul class="text-left">
+					<li>
+						<h3>Direccion:</h3>
+						<p>Joanico 3332</p>
+					</li>
+					<li>
+						<h3>Telefono:</h3>
+						<p>24800409</p>
+					</li>
+					<li>
+						<h3>Celular:</h3>
+						<p>093997007</p>
+					</li>
+				</ul>	
+			</div>
+			<div class="box">
+				<div class="redes-pedido text-center">
+					<h2>HACE TUS PEDIDOS</h2>
+					<a href="https://www.pedidosya.com.uy/restaurantes/montevideo/papin-pizzas-menu" target="'_blank'">
+					<img src="img/pedidoya.png" alt="Logo de pedido ya y enlace al mismo">
+					</a>
+					<a href="https://www.rappi.com.uy/" target="'_blank'">
+					<img src="img/rappi.png" alt="Logo de rappi y enlace al mismo">
+					</a>
+				</div>
+			</div>
+			<div class="box">
+				<h2 class="text-center">SEGUINOS EN REDES</h2>
+					<div class="red-social">
+						<ul class="sociales text-center">
+							<li>
+								<a href="https://www.facebook.com/Papin-Pizzas-145297070652330/" target="'_blank'" class="facebook">
+								<img src="img/facebook.png" alt="Logo de Facebook y enlace al mismo" >
+								</a>
+							</li>
+							<li>
+								<a href="https://www.twitter.com" target="'_blank'" class="twitter">
+								<img src="img/twitter.png" alt="Logo de twitter y enlace al mismo" >	
+								</a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/papinpizzas/?hl=es" target="'_blank'"  class="instagram">
+								<img src="img/instagram.png" alt="Logo de instagram y enlace al mismo ">
+								</a>
+							</li>
+							<li>
+								<a href="https://api.whatsapp.com/send?phone=0123456789" target="'_blank'" class="whatsapp">
+									<img src="img/whatsapp.png" alt="Logo de whatsapp y enlace a whatsapp web" >
+								</a>
+							</li>
+						</ul>
+					</div>
+			</div>	
+</footer>
+<!--termina footer-->	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
