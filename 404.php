cfb922f36f7378fe46d746f231bf0921ee6ce161
guillesangio <guillesangio@gmail.com>
2022-08-02 20:52:35 -0300

    404

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@
 	<nav class="navbar navbar-expand-lg bg-black sticky-top">
 	  	<div class="container-fluid">
 	    	<a class="navbar-brand" href="index.html">
-	    	<img src="img/logo.svg"  alt="logo de Papin Pizzas - Comer como en casa" id="img_logo">
+	    	<img src="../img/logo.svg"  alt="logo de Papin Pizzas - Comer como en casa" id="img_logo">
 	    	</a>
 	    	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    	<span class="navbar-toggler-icon"></span>
@@ -43,13 +43,13 @@
 		<h3>la cocina si   !</h3>
 		<h3>Te esperamos en el inicio nuevamente</h3>
 	</div>
-		<a href="index.html"	class="boton_index">VOLVE AL INICIO</a>
+		<a href="../index.html"	class="boton_index">VOLVE AL INICIO</a>
 </main>
 	<footer class="pie-pagina" id="pie">
 		<div class="logo_footer">
 			<div class="box1">
 				<a href="index.html">
-					<img src="img/logo.png" alt="logo de Papin Pizzas - Comer como en casa" class="logo_index" id="logo">
+					<img src="../img/logo.png" alt="logo de Papin Pizzas - Comer como en casa" class="logo_index" id="logo">
 				</a>
 			</div>
 			<div class="box" class="contacto-papin">
@@ -73,10 +73,10 @@
 				<div class="redes-pedido text-center">
 					<h2>HACE TUS PEDIDOS</h2>
 					<a href="https://www.pedidosya.com.uy/restaurantes/montevideo/papin-pizzas-menu" target="'_blank'">
-					<img src="img/pedidoya.png" alt="Logo de pedido ya y enlace al mismo">
+					<img src="../img/pedidoya.png" alt="Logo de pedido ya y enlace al mismo">
 					</a>
 					<a href="https://www.rappi.com.uy/" target="'_blank'">
-					<img src="img/rappi.png" alt="Logo de rappi y enlace al mismo">
+					<img src="../img/rappi.png" alt="Logo de rappi y enlace al mismo">
 					</a>
 				</div>
 			</div>
@@ -86,22 +86,22 @@
 						<ul class="sociales text-center">
 							<li>
 								<a href="https://www.facebook.com/Papin-Pizzas-145297070652330/" target="'_blank'" class="facebook">
-								<img src="img/facebook.png" alt="Logo de Facebook y enlace al mismo" >
+								<img src="../img/facebook.png" alt="Logo de Facebook y enlace al mismo" >
 								</a>
 							</li>
 							<li>
 								<a href="https://www.twitter.com" target="'_blank'" class="twitter">
-								<img src="img/twitter.png" alt="Logo de twitter y enlace al mismo" >	
+								<img src="../img/twitter.png" alt="Logo de twitter y enlace al mismo" >	
 								</a>
 							</li>
 							<li>
 								<a href="https://www.instagram.com/papinpizzas/?hl=es" target="'_blank'"  class="instagram">
-								<img src="img/instagram.png" alt="Logo de instagram y enlace al mismo ">
+								<img src="../img/instagram.png" alt="Logo de instagram y enlace al mismo ">
 								</a>
 							</li>
 							<li>
 								<a href="https://api.whatsapp.com/send?phone=0123456789" target="'_blank'" class="whatsapp">
-									<img src="img/whatsapp.png" alt="Logo de whatsapp y enlace a whatsapp web" >
+									<img src="../img/whatsapp.png" alt="Logo de whatsapp y enlace a whatsapp web" >
 								</a>
 							</li>
 						</ul>
